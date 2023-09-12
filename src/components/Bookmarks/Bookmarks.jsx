80a8c78f969280a531032ae89ff858d7a0e6c84e
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-12 14:45:48 +0530

    proptypes added

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Bookmarks = () => {
+    return (
+        <div className='w-1/3 bg-cyan-300 mr-4'>
+            <p>book</p>
+        </div>
+    );
+};
+
+export default Bookmarks;
